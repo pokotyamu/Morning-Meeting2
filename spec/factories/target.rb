aef47a026a36775012f87d6cf1e47047cfb68270
pokotyamu <pokotyamu@gmail.com>
2019-02-11 17:46:37 +0900

    fix: remove unnecessary block args.

--- spec/factories/target.rb
+++ spec/factories/target.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :target do
     team
-    sequence(:start_on) { |n| Date.today.beginning_of_month }
-    value { |n| 100_000_000 }
+    start_on { Date.today.beginning_of_month }
+    value { 100_000_000 }
   end
 end
