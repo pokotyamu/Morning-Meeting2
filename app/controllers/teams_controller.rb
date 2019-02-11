53b2fdb392f2bd3c01381e28282f5e71eb908acc
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2019-02-11 16:05:25 +0900

    team の new と create アクションを実装した

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -4,4 +4,19 @@ class TeamsController < ApplicationController
     @enabled_teams = teams.enabled
     @disabled_teams = teams.disabled
   end
+
+  def new
+  end
+
+  def create
+    Team.create(team_params)
+    flash[:notice] = "チーム作成が成功しました！"
+    redirect_to root_url
+  end
+
+  private
+
+  def team_params
+    params.require(:team).permit(:name, :order)
+  end
 end
