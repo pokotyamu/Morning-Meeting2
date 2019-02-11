c47ccc53a7498284c88b1a129fe7d75658cd1505
pokotyamu <pokotyamu@gmail.com>
2019-02-11 17:50:31 +0900

    feat: set default 0 for performances.value

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -6,7 +6,7 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  start_on   :datetime         not null
-#  value      :integer          not null
+#  value      :integer          default(0), not null
 #  content    :text             not null
 #  target_id  :integer          not null
 #
