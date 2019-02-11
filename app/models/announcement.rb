27677e0abdb535a7894fd5a7e5dc0db8db57ac4c
pokotyamu <pokotyamu@gmail.com>
2019-02-11 16:17:08 +0900

    feat: add announcements.start_on

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -4,6 +4,7 @@
 #
 #  id         :integer          not null, primary key
 #  content    :text             not null
+#  start_on   :datetime         not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
